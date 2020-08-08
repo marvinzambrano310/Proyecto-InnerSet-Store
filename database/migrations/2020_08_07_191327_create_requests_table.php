0342e366d5fc1a3d318530ed34a3413a396e85a3
EstebanRios99 <esteban.rios@epn.edu.ec>
2020-08-07 20:04:05 -0500

    #1 Modelo y migraciones

---
+++ database/migrations/2020_08_07_191327_create_requests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('date');
+            $table->double('subtotal');
+            $table->enum('type',['withdraw','deliver']);
+            $table->double('surcharge');
+            $table->double('total');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('requests');
+    }
+}
