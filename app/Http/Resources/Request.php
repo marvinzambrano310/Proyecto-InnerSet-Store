10beb48de44af5a9a3d39d66d52300e751affff5
marvinzambrano310 <marvin.zambrano@epn.edu.ec>
2021-03-11 18:58:39 -0500

    datos del usuario y tipo de usuario dentro de recurso del Pedido

--- app/Http/Resources/Request.php
+++ app/Http/Resources/Request.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\User;
+use App\Client;
 
 class Request extends JsonResource
 {
@@ -16,10 +17,14 @@ class Request extends JsonResource
     public function toArray($request)
     {
         $user=User::where('id', $this->user_id)->first();
+        //$data=$this->user_id-1;
+        $usertype=Client::where('id', $user->userable_id)->first();
+
         return [
             'id' => $this->id,
             //'user' => "/api/users/" . $this->user_id,
             'user' => $user,
+            'usertype' => $usertype,
             'date' => $this->date,
             //'detail'=>"/api/details".$this->detail,
             'detail'=>(new DetailRequestCollection($this->detail)),
