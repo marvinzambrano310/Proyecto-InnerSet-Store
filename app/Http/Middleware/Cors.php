b55e69835ef4ab2af776fdb8f54dd09830e71d13
marvinzambrano310 <marvin.zambrano@epn.edu.ec>
2021-03-23 23:22:33 -0500

    Cambios Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,8 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Origin, Accept, X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+            ->header("Access-Control-Allow-Credentials", "true")
+            ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+            ->header("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, X-Token-Auth, Authorization");
     }
 }
