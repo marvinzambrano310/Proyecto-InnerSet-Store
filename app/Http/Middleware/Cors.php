eb86225bf1a890aed22d8991e6f9c8b3ad5ceea1
marvinzambrano310 <marvin.zambrano@epn.edu.ec>
2021-03-24 11:10:37 -0500

    Cambios Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,8 +17,8 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', 'https://proyecto-inner-set-store-olosd.ondigitalocean.app/api')
-            ->header("Access-Control-Allow-Credentials", "true")
-            ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-            ->header("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Origin, Accept, X-Requested-With, Content-Type, X-Token-Auth, Authorization');
     }
 }
